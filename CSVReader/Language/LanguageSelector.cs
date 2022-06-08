d5660c972021572b647a6d53006be32f7808bd0c
Руслан Топольцев <grant99458@gmail.com>
2022-06-08 22:05:42 +0300

    Add project files.

---
+++ CSVReader/Language/LanguageSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+namespace CSVReader.Language
+{
+    internal class LanguageSelector
+    {
+        public Dictionary<string, string> Languages { get; private set; }
+
+        public LanguageSelector()
+        {
+            Languages = new Dictionary<string, string>()
+            {
+                { "English", "en-US" },
+                { "Русский", "ru-RU" }
+            };
+        }
+
+        public void Select(string key)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(Languages[key]);
+        }
+    }
+}
---
+++ CSVReader/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CSVReader.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSVReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LanguageSelector _languageSelector;
+        private string[] _languages;
+
+        private const int DefaultLangeageIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _languageSelector = new LanguageSelector();
+            _languages = _languageSelector.Languages.Keys.ToArray();
+            _languageSelector.Select(_languages[DefaultLangeageIndex]);
+
+            Settings.Header = InterfaceLanguage.Settings;
+            Language1.Header = InterfaceLanguage.Language;
+        }
+
+        private void Language_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuElements.Settings.Settings settingsWindow = new MainMenuElements.Settings.Settings(_languages, DefaultLangeageIndex);
+            settingsWindow.ShowDialog();
+        }
+    }
+}
