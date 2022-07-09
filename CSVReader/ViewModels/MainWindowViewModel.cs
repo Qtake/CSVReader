662f00a398e41e8893566fef61b565f8886bfd30
Руслан Топольцев <grant99458@gmail.com>
2022-07-09 18:19:59 +0300

    Completed filtration

--- CSVReader/ViewModels/MainWindowViewModel.cs
+++ CSVReader/ViewModels/MainWindowViewModel.cs
@@ -14,7 +14,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,25 +29,90 @@ namespace CSVReader.ViewModels
         public ICommand ExitCommand { get; init; }
         public ICommand SettingsWindowCommand { get; init; }
         public ICommand FiltrationWindowCommand { get; init; }
+        public ICommand ApplyFiltersCommand { get; init; }
+        public ICommand ResetFiltersCommand { get; init; }
         public Page CurrentFramePage { get; private set; }
         public List<Record> DataBaseRecords { get; private set; }
         public bool OpenButtonState { get; private set; }
         public bool SaveAsButtonState { get; private set; }
 
+        public DateTime? Date
+        {
+            get => _filter.Date;
+            set
+            {
+                _filter.Date = value;
+                RaisePropertyChanged(nameof(Date));
+            }
+        }
+
+        public string Firstname
+        {
+            get => _filter.Firstname;
+            set
+            {
+                _filter.Firstname = value;
+                RaisePropertyChanged(nameof(Firstname));
+            }
+        }
+
+        public string Surname
+        {
+            get => _filter.Surname;
+            set
+            {
+                _filter.Surname = value;
+                RaisePropertyChanged(nameof(Surname));
+            }
+        }
+
+        public string Patronymic
+        {
+            get => _filter.Patronymic;
+            set
+            {
+                _filter.Patronymic = value;
+                RaisePropertyChanged(nameof(Patronymic));
+            }
+        }
+
+        public string City
+        {
+            get => _filter.City;
+            set
+            {
+                _filter.City = value;
+                RaisePropertyChanged(nameof(City));
+            }
+        }
+
+        public string Country
+        {
+            get => _filter.Country;
+            set
+            {
+                _filter.Country = value;
+                RaisePropertyChanged(nameof(Country));
+            }
+        }
+
         private IRepository<Record>? _repository;
         private IReader? _dataReader;
         private IWriter? _dataWriter;
+        private Record _filter;
         private const string DefaultFileName = "Document";
         
         public MainWindowViewModel()
         {
             SetDataBaseConnection();
 
-            OpenCommand = new RelayCommand((x) => OpenFile());
-            SaveAsCommand = new RelayCommand((x) => SaveAsFile());
+            OpenCommand = new RelayCommand((x) => OpenFileAsync());
+            SaveAsCommand = new RelayCommand((x) => SaveAsFileAsync());
             ExitCommand = new RelayCommand((x) => CloseApplication());
             SettingsWindowCommand = new RelayCommand((x) => ShowSettingsWindow());
             FiltrationWindowCommand = new RelayCommand((x) => ShowFiltrationWindow());
+            ApplyFiltersCommand = new RelayCommand((x) => ApplyFiltersAsync());
+            ResetFiltersCommand = new RelayCommand((x) => ResetFiltersAsync());
             _repository = null;
             _dataReader = null;
             _dataWriter = null;
@@ -56,9 +120,15 @@ namespace CSVReader.ViewModels
             DataBaseRecords = null!;
             OpenButtonState = true;
             SaveAsButtonState = false;
+            _filter = new Record();
         }
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void RaisePropertyChanged(string prop)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
@@ -92,7 +162,7 @@ namespace CSVReader.ViewModels
             OnPropertyChanged(nameof(CurrentFramePage));
         }
 
-        private async void OpenFile()
+        private async void OpenFileAsync()
         {
             EnableMenuElements(false);
             DeletePreviousData();
@@ -119,7 +189,7 @@ namespace CSVReader.ViewModels
             OnPropertyChanged(nameof(OpenButtonState));
         }
 
-        private async void SaveAsFile()
+        private async void SaveAsFileAsync()
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog()
             {
@@ -161,8 +231,61 @@ namespace CSVReader.ViewModels
         {
             using (_repository = new MsSqlRepository(ApplicationSettings.Default.DatabaseConnectionString))
             {
-                await Task.Run(() => DataBaseRecords = _repository.SelectAll().ToList());
+                DataBaseRecords = await _repository.SelectAll().ToListAsync();
             }
+        }
+
+        private async void ApplyFiltersAsync()
+        {
+            using (_repository = new MsSqlRepository(ApplicationSettings.Default.DatabaseConnectionString))
+            {
+                IQueryable<Record> records = _repository.SelectAll();
+
+                if (_filter.Date != null)
+                {
+                    records = _repository.SelectAll(x => x.Date == Date);
+                }
+
+                if (!string.IsNullOrEmpty(Firstname))
+                {
+                    records = _repository.SelectAll(x => x.Firstname == Firstname);
+                }
+
+                if (!string.IsNullOrEmpty(Surname))
+                {
+                    records = _repository.SelectAll(x => x.Surname == Surname);
+                }
+
+                if (!string.IsNullOrEmpty(Patronymic))
+                {
+                    records = _repository.SelectAll(x => x.Patronymic == Patronymic);
+                }
+
+                if (!string.IsNullOrEmpty(City))
+                {
+                    records = _repository.SelectAll(x => x.City == City);
+                }
+
+                if (!string.IsNullOrEmpty(Country))
+                {
+                    records = _repository.SelectAll(x => x.Country == Country);
+                }
+
+                DataBaseRecords = await records.ToListAsync();
+                UpdateSavedRecords();
+            }
+        }
+
+        private async void ResetFiltersAsync()
+        {
+            _filter = new Record();
+            await GetDataBaseRecordsAsync();
+            UpdateSavedRecords();
+        }
+
+        private void UpdateSavedRecords()
+        {
+            OnPropertyChanged(nameof(DataBaseRecords));
         }
     }
 }
